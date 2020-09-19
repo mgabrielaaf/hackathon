a008da0b2f1f7c03ee91b80f38e929f37dfe1519
mgabrielaaf <mgabrielaaurelianof@gmail.com>
2020-09-19 11:13:18 -0300

    pages

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuPageLayoutComponent } from './components/menu-page-layout/menu-page-layout.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './components/home/home.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { InserirComponent } from './components/inserir/inserir.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPageLayoutComponent,
+    HomeComponent,
+    FeedComponent,
+    InserirComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
